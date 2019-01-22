951c54356505d21fde62e9e5b5d445cf5f0cb0c7
bre1470 <40056287+bre1470@users.noreply.github.com>
2019-01-22 16:41:58 +0100

    TypeDoc: Added value driven conditional statements.

--- typedoc/theme/helpers/CamelCase.js
+++ typedoc/theme/helpers/CamelCase.js
@@ -1,17 +1,40 @@
+/*
+ * Copyright (C) Highsoft AS
+ */
+
+/**
+ * Changes the case of a name with dashes or low dashes.
+ *
+ * @param {string} name 
+ *
+ * @return {string}
+ */
+function changeCase (name) {
+    return name.replace(
+        /[\-_][a-z]/g,
+        match => match[1].toUpperCase()
+    );
+}
+
 module.exports = {
-    CamelCase: function(name) {
+
+    CamelCase: function (name) {
+
         if (!name) {
             return name;
         }
-        return (
-            name[0].toUpperCase() +
-            name.replace(/[\-_][a-z]/g, match => match[1].toUpperCase())
-        );
+
+        name = changeCase(name);
+
+        return name[0].toUpperCase() + name.substr(1);
     },
-    camelCase: function(name) {
+
+    camelCase: function (name) {
+
         if (!name) {
             return name;
         }
-        return name.replace(/[\-_][a-z]/g, match => match[1].toUpperCase());
+
+        return changeCase(name);
     }
 }
