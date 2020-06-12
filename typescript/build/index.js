4b66af49c01816a8585cf4dcb7c9689088410edf
bre1470 <40056287+bre1470@users.noreply.github.com>
2020-06-12 11:29:08 +0200

    Fixed file filtering.

--- typescript/build/index.js
+++ typescript/build/index.js
@@ -1,11 +1,19 @@
 "use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./JSONUtilities"));
-__export(require("./Members/"));
-__export(require("./MembersUtilities"));
-__export(require("./Project"));
-__export(require("./Types/"));
-__export(require("./TypesUtilities"));
+__exportStar(require("./JSON/"), exports);
+__exportStar(require("./JSONUtilities"), exports);
+__exportStar(require("./Members/"), exports);
+__exportStar(require("./MembersUtilities"), exports);
+__exportStar(require("./Project"), exports);
+__exportStar(require("./Types/"), exports);
+__exportStar(require("./TypesUtilities"), exports);
